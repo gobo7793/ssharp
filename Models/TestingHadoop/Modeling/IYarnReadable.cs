3d26507f2deb62ab3fd6a047cc019095fda40f9e
Gerald Siegert <gerald.siegert@outlook.com>
2018-01-05 15:50:29 +0100

    added interface for radable components

---
+++ Models/TestingHadoop/Modeling/IYarnReadable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SafetySharp.CaseStudies.TestingHadoop
+{
+    /// <summary>
+    /// Interface for YARN components which can read its state from hadoop
+    /// </summary>
+    public interface IYarnReadable
+    {
+        /// <summary>
+        /// Reads the current status from Hadoop
+        /// </summary>
+        void GetStatus();
+    }
+}
