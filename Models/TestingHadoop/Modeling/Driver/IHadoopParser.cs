2a9aeab852b515fedc440d08a8107aa563eb44ce
Gerald Siegert <gerald.siegert@outlook.com>
2018-01-07 16:40:01 +0100

    created parser interface

---
+++ Models/TestingHadoop/Modeling/Driver/IHadoopParser.cs
@@ -0,0 +1,99 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014-2018, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using SafetySharp.CaseStudies.TestingHadoop.Modeling.HadoopModel;
+
+namespace SafetySharp.CaseStudies.TestingHadoop.Modeling.Driver
+{
+    /// <summary>
+    /// Interface for Hadoop Parser
+    /// </summary>
+    public interface IHadoopParser
+    {
+        #region Parse Lists
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnApp"/> list for the given <see cref="EAppState"/>s.
+        /// </summary>
+        /// <param name="states">The states</param>
+        /// <returns>The applications</returns>
+        ApplicationListResult[] ParseAppList(params EAppState[] states);
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnAppAttempt"/> list for the given <see cref="YarnApp.AppId"/>
+        /// </summary>
+        /// <param name="appId">The app id</param>
+        /// <returns>The attempts</returns>
+        ApplicationAttemptListResult[] ParseAttemptAttemptList(string appId);
+
+        /// <summary>
+        /// Gets and parses the current running <see cref="YarnAppContainer"/> list for the given <see cref="YarnAppAttempt.AttemptId"/>
+        /// </summary>
+        /// <param name="attemptId">The attempt id</param>
+        /// <returns>The running containers</returns>
+        ContainerListResult[] ParseContainerList(string attemptId);
+
+        #endregion
+
+        #region Parse Details
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnApp"/> details
+        /// </summary>
+        /// <param name="appId">The <see cref="YarnApp.AppId"/> from the app</param>
+        /// <returns>The application details</returns>
+        ApplicationDetailsResult ParseAppDetails(string appId);
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnAppAttempt"/> details
+        /// </summary>
+        /// <param name="attemptId">The <see cref="YarnAppAttempt.AttemptId"/> from the attempt</param>
+        /// <returns>The attempt details</returns>
+        ApplicationAttemptDetailsResult ParseAttemptAppDetails(string attemptId);
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnAppContainer"/> details
+        /// </summary>
+        /// <param name="containerId">The <see cref="YarnAppContainer.ContainerId"/> from the container</param>
+        /// <returns>The container details</returns>
+        ContainerListResult ParseContainerDetails(string containerId);
+
+        #endregion
+
+        #region Parse Nodes
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnNode"/> list for the cluster
+        /// </summary>
+        /// <returns>All nodes in the cluster</returns>
+        NodeListResult ParseNodeList();
+
+        /// <summary>
+        /// Gets and parses the <see cref="YarnNode"/> details
+        /// </summary>
+        /// <param name="nodeId">The <see cref="YarnNode.NodeId"/> from the node</param>
+        /// <returns>The node details</returns>
+        NodeDetailsResult ParseNodeDetails(string nodeId);
+
+        #endregion
+    }
+}
