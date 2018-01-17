4e92a80027155183e3ea1c1759c52673a96aedc8
Gerald Siegert <gerald.siegert@outlook.com>
2018-01-17 11:29:52 +0100

    moved parser utilities test to own class

---
+++ Models/TestingHadoop/Tests/ParserUtilitiesTest.cs
@@ -0,0 +1,40 @@
+﻿// The MIT License (MIT)
+// 
+// Copyright (c) 2014-2018, Institute for Software & Systems Engineering
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using NUnit.Framework;
+using SafetySharp.CaseStudies.TestingHadoop.Modeling.Driver;
+
+namespace SafetySharp.CaseStudies.TestingHadoop.Tests
+{
+    [TestFixture]
+    public class ParserUtilitiesTest
+    {
+        [Test]
+        [TestCase("Wed Jan 10 19:42:01 +0000 2018", CmdLineParser.HadoopDateFormat, Result = "2018-01-10T20:42:01.0000000+01:00")]
+        [TestCase("1512187108523", null, Result = "2017-12-02T04:58:28.5230000+01:00")]
+        [TestCase("0", null, Result = "0001-01-01T00:00:00.0000000")]
+        public string TestParseTimestamp(string date, string format)
+        {
+            return ParserUtilities.ParseJavaTimestamp(date, format).ToString("o");
+        }
+    }
+}
