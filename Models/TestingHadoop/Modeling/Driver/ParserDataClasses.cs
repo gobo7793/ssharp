1326757c0481f590937f1bb80ec70861eb78c31e
Gerald Siegert <gerald.siegert@outlook.com>
2018-01-18 14:28:38 +0100

    added new properties from json files

--- Models/TestingHadoop/Modeling/Driver/ParserDataClasses.cs
+++ Models/TestingHadoop/Modeling/Driver/ParserDataClasses.cs
@@ -113,6 +113,24 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Modeling.Driver
         public string AmHostHttpAddress { get; set; }
 
         /// <summary>
+        /// Running containers count
+        /// </summary>
+        [JsonProperty("runningContainers")]
+        public long RunningContainers { get; set; }
+
+        /// <summary>
+        /// Allocated Memory in MB
+        /// </summary>
+        [JsonProperty("allocatedMB")]
+        public long AllocatedMb { get; set; }
+
+        /// <summary>
+        /// Allocated CPU in VCores
+        /// </summary>
+        [JsonProperty("allocatedVCores")]
+        public long AllocatedVcores { get; set; }
+
+        /// <summary>
         /// Aggregate Resource Allocation MB-seconds
         /// </summary>
         [JsonProperty("memorySeconds")]
@@ -124,6 +142,30 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Modeling.Driver
         [JsonProperty("vcoreSeconds")]
         public long VcoreSeconds { get; set; }
 
+        /// <summary>
+        /// Preempted Memory in MB
+        /// </summary>
+        [JsonProperty("preemptedResourceMB")]
+        public long PreemptedMb { get; set; }
+
+        /// <summary>
+        /// Preempted CPU in VCores
+        /// </summary>
+        [JsonProperty("preemptedResourceVCores")]
+        public long PreemptedVcores { get; set; }
+
+        /// <summary>
+        /// Preempted Non-AM container count
+        /// </summary>
+        [JsonProperty("numNonAMContainerPreempted")]
+        public long NonAmContainerPreempted { get; set; }
+
+        /// <summary>
+        /// Preempted AM container count
+        /// </summary>
+        [JsonProperty("allocatedVCores")]
+        public long AmContainerPreempted { get; set; }
+
         public override bool Equals(object obj)
         {
             var result = obj as ApplicationResult;
@@ -133,19 +175,26 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Modeling.Driver
                    AppType == result.AppType &&
                    State == result.State &&
                    FinalStatus == result.FinalStatus &&
-                   Math.Abs(Progess - result.Progess) < 0.01 &&
+                   Progess == result.Progess &&
                    TrackingUrl == result.TrackingUrl &&
                    StartTime == result.StartTime &&
                    FinishTime == result.FinishTime &&
                    EqualityComparer<YarnNode>.Default.Equals(AmHost, result.AmHost) &&
                    AmHostHttpAddress == result.AmHostHttpAddress &&
+                   RunningContainers == result.RunningContainers &&
+                   AllocatedMb == result.AllocatedMb &&
+                   AllocatedVcores == result.AllocatedVcores &&
                    MbSeconds == result.MbSeconds &&
-                   VcoreSeconds == result.VcoreSeconds;
+                   VcoreSeconds == result.VcoreSeconds &&
+                   PreemptedMb == result.PreemptedMb &&
+                   PreemptedVcores == result.PreemptedVcores &&
+                   NonAmContainerPreempted == result.NonAmContainerPreempted &&
+                   AmContainerPreempted == result.AmContainerPreempted;
         }
 
         public override int GetHashCode()
         {
-            var hashCode = -1000931503;
+            var hashCode = -2097537572;
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(AppId);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(AppName);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(AppType);
@@ -157,8 +206,15 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Modeling.Driver
             hashCode = hashCode * -1521134295 + FinishTime.GetHashCode();
             hashCode = hashCode * -1521134295 + EqualityComparer<YarnNode>.Default.GetHashCode(AmHost);
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(AmHostHttpAddress);
+            hashCode = hashCode * -1521134295 + RunningContainers.GetHashCode();
+            hashCode = hashCode * -1521134295 + AllocatedMb.GetHashCode();
+            hashCode = hashCode * -1521134295 + AllocatedVcores.GetHashCode();
             hashCode = hashCode * -1521134295 + MbSeconds.GetHashCode();
             hashCode = hashCode * -1521134295 + VcoreSeconds.GetHashCode();
+            hashCode = hashCode * -1521134295 + PreemptedMb.GetHashCode();
+            hashCode = hashCode * -1521134295 + PreemptedVcores.GetHashCode();
+            hashCode = hashCode * -1521134295 + NonAmContainerPreempted.GetHashCode();
+            hashCode = hashCode * -1521134295 + AmContainerPreempted.GetHashCode();
             return hashCode;
         }
     }
