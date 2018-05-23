7dca4657060b7666959436b1891b4ba8e81d00cc
Gerald Siegert <gerald.siegert@outlook.com>
2018-05-23 12:17:29 +0200

    sleep time set to 4 secs

--- Models/TestingHadoop/Modeling/HadoopModel/YarnController.cs
+++ Models/TestingHadoop/Modeling/HadoopModel/YarnController.cs
@@ -105,7 +105,7 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Modeling.HadoopModel
                 client.UpdateBenchmark();
 
             // optional, to allocate at least the AM container
-            ModelUtilities.Sleep(3);
+            ModelUtilities.Sleep(4);
 
             MonitorNodes();
             MonitorApps();
