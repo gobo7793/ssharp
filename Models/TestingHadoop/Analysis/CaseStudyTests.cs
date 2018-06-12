6b50417aac6b19de1a73bbb9027d79297a63a8ae
Gerald Siegert <gerald.siegert@outlook.com>
2018-06-12 11:11:53 +0200

    changed log file naming

--- Models/TestingHadoop/Analysis/CaseStudyTests.cs
+++ Models/TestingHadoop/Analysis/CaseStudyTests.cs
@@ -314,10 +314,10 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Analysis
 
             var caseStudyLogDir = $@"{Environment.CurrentDirectory}\testingHadoopCaseStudyLogs";
             var todayStrShort = DateTime.Today.ToString("yyMMdd");
-            var mutatedInt = isMutated ? 1 : 0;
+            var mutatedInt = isMutated ? 'T' : 'F';
             var faultProbStr = faultProbability.ToString(CultureInfo.InvariantCulture);
             var filename = $"{benchmarkSeed:X8}-{faultProbStr}-{hostsCount:D1}-" +
-                           $"{clientCount:D1}-{stepCount:D2}-{mutatedInt:D1}-{todayStrShort}";
+                           $"{clientCount:D1}-{stepCount:D2}-{mutatedInt}-{todayStrShort}";
             var newLogFile = $@"{caseStudyLogDir}\{filename}.log";
             var newSshLog = $@"{caseStudyLogDir}\{filename}-ssh.log";
 
