89c12fe9eca73c8c83d5e582b3db2ee9af322767
Gerald Siegert <gerald.siegert@outlook.com>
2018-06-12 11:17:25 +0200

    better readable test cases

--- Models/TestingHadoop/Analysis/CaseStudyTests.cs
+++ Models/TestingHadoop/Analysis/CaseStudyTests.cs
@@ -87,12 +87,12 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Analysis
         public IEnumerable GetTestCases()
         {
             foreach(var seed in GetSeeds())
-                foreach(var prob in GetFaultProbabilities())
-                    foreach(var hosts in GetHostCounts())
-                        foreach(var clients in GetClientCounts())
-                            foreach(var steps in GetStepCounts())
-                                foreach(var isMut in GetIsMutated())
-                                    yield return new TestCaseData(seed, prob, hosts, clients, steps, isMut);
+            foreach(var prob in GetFaultProbabilities())
+            foreach(var hosts in GetHostCounts())
+            foreach(var clients in GetClientCounts())
+            foreach(var steps in GetStepCounts())
+            foreach(var isMut in GetIsMutated())
+                yield return new TestCaseData(seed, prob, hosts, clients, steps, isMut);
         }
 
         /// <summary>
