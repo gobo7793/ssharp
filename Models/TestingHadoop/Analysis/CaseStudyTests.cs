a3298c602255701fb6f5a8166b23578bc48c39b5
Gerald Siegert <gerald.siegert@outlook.com>
2018-06-13 14:57:05 +0200

    get 2 test seeds

--- Models/TestingHadoop/Analysis/CaseStudyTests.cs
+++ Models/TestingHadoop/Analysis/CaseStudyTests.cs
@@ -74,12 +74,11 @@ namespace SafetySharp.CaseStudies.TestingHadoop.Analysis
             var ran = new Random(ticks);
             var s1 = ran.Next(0, int.MaxValue);
             var s2 = ran.Next(0, int.MaxValue);
-            var s3 = ran.Next(0, int.MaxValue);
             Console.WriteLine($"Ticks: 0x{ticks:X}");
-            Console.WriteLine($"s1: 0x{s1:X} | s2: 0x{s2:X} | s3: 0x{s3:X}");
+            Console.WriteLine($"s1: 0x{s1:X} | s2: 0x{s2:X}");
             // Specific output for generating test case seeds:
             // Ticks: 0x719E8C
-            // s1: 0xE99032B | s2: 0x4F009539 | s3: 0x319140E0
+            // s1: 0xE99032B | s2: 0x4F009539
         }
 
         #endregion
